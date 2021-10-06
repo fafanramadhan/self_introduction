0564d026c72810af37cc617955ec4def7c563a88
fafanramadhan <79952732+fafanramadhan@users.noreply.github.com>
2021-10-06 23:17:30 +0700

    upload my program

---
+++ 01_Pertemuan 1/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;                                                 //buat nyingkat std::
+
+int main()
+{
+   cout<<"============ \n";                                           // \n biar ke baris baru kaya endl
+   cout<<"Hello World \n";
+   cout<<"============ \n";
+   cout<<"Nama      : Rachmad Fajar Ramadhan \n";
+   cout<<"Nim       : 21.11.4418 \n";
+   cout<<"jurusan   : Informatika \n"; 
+   cout<<"Asal      : Yogyakarta \n";
+   cin.get();                                                         //biar program ngga flashing atau nutup sendiri
+}
+
